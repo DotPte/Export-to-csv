ac5fb77d9b829818757a2bb7c30474c5c30ec504
Petja Väisänen <vaisanen.petja@gmail.com>
2017-03-30 11:27:44 +0300

    Create export.php

---
+++ export.php
@@ -0,0 +1,38 @@
+<?php if (isset($_POST['exp'])) {
+
+    header('Content-Type: text/csv; charset=utf-8');
+    
+// filename specifies name you want to file be named when exporting/downloading csv file
+    header('Content-Disposition: attachment; filename=export.csv');
+
+    $output = fopen('php://output', 'w');
+
+// this is where you name first row of the csv see readme.md
+  fputcsv($output, array('Search engine', 'Why', 'Useful'));
+	
+// connecting to server exsamble ('localhost', 'User', 'mypassword', 'DefaultDataBase');
+    $con = mysqli_connect('Server', 'Username', 'Password', 'database');
+
+// this specifies rows you want to include to the csv
+// after "SELECT you name columns you want and after FROM table you want to export
+// if you want to export all columns use SELECT *
+// if you want to export all tables use FROM * (i thinks it works? if it doesn`t work it exports empty csv with first row specified in line 11)
+    
+    $rows = mysqli_query($con, "SELECT search engine, useful, why FROM search engines ");
+
+    while ($row = mysqli_fetch_assoc($rows)) {
+      fputcsv($output, $row);
+    }
+    fclose($output);
+    mysqli_close($con);
+    exit();
+  }
+?>
+
+// this is just HTML code that adds export button to the site, you can get rid of it if you feel like you dont need it
+
+<div>
+  <form action="#" method="post">
+    <input type="submit" value="Export" name="exp" />
+  </form>
+</div>
