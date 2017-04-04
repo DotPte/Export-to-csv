8f9c437478b192c43e80df459dbe5bf684138e4a
Petja Väisänen <vaisanen.petja@gmail.com>
2017-04-04 16:08:40 +0300

    Update Export-from-multible-tables.php

--- Export-from-multible-tables.php
+++ Export-from-multible-tables.php
@@ -8,9 +8,12 @@
     $rows = mysqli_query($con, "SELECT table1.row1, table2.row3, table3.row5, table2.row1 FROM table1 ");
     INNER JOIN table2 ON table1.row1 = table2.row2
     INNER JOIN table3 ON table1.row5 = table3.row4
+    ");
+   
     while ($row = mysqli_fetch_assoc($rows)) {
     fputcsv($output, $row);
     }
+    
     fclose($output);
     mysqli_close($con);
     exit();
